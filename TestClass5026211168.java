a743cd41e91111e7fbb1f137d83cf359d2ce3a9a
Luthfi Hakim <100079246+LuthfiHakimI@users.noreply.github.com>
2022-03-01 12:43:48 +0700

    Add files via upload

---
+++ TestClass5026211168.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class TestClass5026211168{
+
+	public static void main(String[] args){
+	
+	ComputerMethods5026211168 compMeth = new ComputerMethods5026211168();
+	
+	//F to C
+	compMeth.fToC(80);
+	
+	//calt Hypotenuse Lenght
+	compMeth.hypotenuse(3,4);
+
+	//Dice sum of 2 dice
+	compMeth.roll();
+
+	}
+}
